e6b329be161ef995ef1b51409289889475b647bb
Morty Ray <amortya.ray@common.com>
2020-02-04 10:12:48 -0500

    Change session store to redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,9 @@
-Rails.application.config.session_store :cookie_store, key: '_rails_session_demo_app', expire_after: 3.days
-
+Rails.application.config.session_store :redis_session_store, {
+  key: "_rails_redis_session_demo_app",
+  redis: {
+    expire_after: 120.minutes,  # cookie expiration
+    ttl: 120.minutes,           # Redis expiration, defaults to 'expire_after'
+    key_prefix: 'rails_session_demo:session:',
+    url: 'redis://localhost:6379/0'
+  }
+}
