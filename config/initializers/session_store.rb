3e0008509148a9e3e0143ef6ad347d28b856c7b1
Morty Ray <amortya.ray@common.com>
2020-02-05 15:36:17 -0500

    Change session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: '_rails_session_demo_app', expire_after: 3.days
+Rails.application.config.session_store :cookie_store, key: '_rails_cookie_session', expire_after: 3.days
 
